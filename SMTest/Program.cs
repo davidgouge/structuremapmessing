54551c0d9d0f18da3be874b95c51e6f031f7c657
David Gouge <davegouge@gmail.com>
2018-02-08 13:19:03 +0000

    Init

---
+++ SMTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SFA.DAS.AssessmentOrgs.Api.Client;
+using StructureMap;
+
+namespace SMTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new Container(cfg =>
+            {
+                cfg.For<ISomeInterface>().Use<SomeImplementation>();
+            });
+
+            var cli = container.GetInstance<IAssessmentOrgsApiClient>();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public interface ISomeInterface
+    {
+        
+    }
+
+    public class SomeImplementation : ISomeInterface
+    {
+        public SomeImplementation(string arg = null)
+        {
+            
+        }
+    }
+
+}
